58b3053042225c1adeb7ccb1eeb9cea1d7abe19b
Tait Weinert <tait.weinert@outlook.com>
2025-02-04 15:04:35 +1030

    cleaned up code, changed to promise

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 const http = require("http")
 const url = require("url")
-const fs = require("fs")
+const fs = require("fs/promises")
 
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  // res.statusCode = 200;
-  // res.setHeader("Content-Type", "text/plain");
-  // res.end("Hello World");
+const server = http.createServer();
+
+server.on("request", async (req, res) => {
   let q = url.parse(req.url, true);
-  let filename = "." + q.pathname;
-  fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }
+  let filename = q.pathname;
+
+  if (filename === "/") {
+    filename = "./index.html";
+  } else if (filename === "/about") {
+    filename = "./about.html";
+  } else if (filename === "/contact-me") {
+    filename = "./contact-me.html";
+  } else {
+    filename = "./404.html";
+  }
+
+  try {
+    const data = await fs.readFile(filename, { encoding: 'utf8' });
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
-    return res.end();
-  });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 server.listen(port, hostname, () => {
