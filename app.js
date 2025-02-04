0f0905c34d609e3ff9ad874c16e4aa865189058e
Tait Weinert <tait.weinert@outlook.com>
2025-02-04 14:53:41 +1030

    show html files

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const http = require("http")
 const url = require("url")
-const fs = require("fs/promises")
+const fs = require("fs")
+
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  // res.statusCode = 200;
+  // res.setHeader("Content-Type", "text/plain");
+  // res.end("Hello World");
+  let q = url.parse(req.url, true);
+  let filename = "." + q.pathname;
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 });
 
 server.listen(port, hostname, () => {
